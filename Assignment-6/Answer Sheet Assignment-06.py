e79b25ee4b3193f1aed73e811319c53b85b36b89
unknown <bikramnathetc04@gmail.com>
2022-05-01 11:09:08 +0530

    Assignment 6 added

---
+++ Assignment-6/Answer Sheet Assignment-06.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().set_next_input('1. What are escape characters, and how do you use them');get_ipython().run_line_magic('pinfo', 'them')
+--> An escape character is a backslash \ followed by the character you want to insert.
+--> text= "Hi this is \"Vikram\" from delhi."
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('2. What do the escape characters n and t stand for');get_ipython().run_line_magic('pinfo', 'for')
+--> \n is for new line
+--> \t is for horizontal tab.
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('3. What is the way to include backslash characters in a string');get_ipython().run_line_magic('pinfo', 'string')
+--> The backslash ( "\" ) 
+
+
+# In[ ]:
+
+
+4. The string "Howl's Moving Castle" is a correct value. 
+Why isn't the single quote character in the word Howl's not escaped a problem
+
+--> Here we have used double quotes to mark the beginning and end of the string.
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input("5. How do you write a string of newlines if you don't want to use the n character");get_ipython().run_line_magic('pinfo', 'character')
+
+--> It is used to indicate the end of a line of text. You can print strings without adding a new line with 
+end = <character> , which <character> is the character that will be used to separate the lines.
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('6. What are the values of the given expressions');get_ipython().run_line_magic('pinfo', 'expressions')
+'Hello, world!'[1]
+'Hello, world!'[0:5]
+'Hello, world!'[:5]
+'Hello, world!'[3:]
+
+
+# In[7]:
+
+
+'Hello, world!'[1]
+
+
+# In[8]:
+
+
+'Hello, world!'[0:5]
+
+
+# In[9]:
+
+
+'Hello, world!'[:5]
+
+
+# In[10]:
+
+
+'Hello, world!'[3:]
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('7. What are the values of the following expressions');get_ipython().run_line_magic('pinfo', 'expressions')
+'Hello'.upper()
+'Hello'.upper().isupper()
+'Hello'.upper().lower()
+
+
+# In[11]:
+
+
+'Hello'.upper()
+
+
+# In[12]:
+
+
+'Hello'.upper().isupper()
+
+
+# In[13]:
+
+
+'Hello'.upper().lower()
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('8. What are the values of the following expressions');get_ipython().run_line_magic('pinfo', 'expressions')
+'Remember, remember, the fifth of July.'.split()
+'-'.join('There can only one.'.split())
+
+
+# In[14]:
+
+
+'Remember, remember, the fifth of July.'.split()
+
+
+# In[17]:
+
+
+'-'.join('There can only one.'.split())
+
+
+# In[36]:
+
+
+get_ipython().set_next_input('9. What are the methods for right-justifying, left-justifying, and centering a string');get_ipython().run_line_magic('pinfo', 'string')
+
+left justifying ---> ljust()
+right justifying ---> rjust()
+center justifying ---> center()
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('10. What is the best way to remove whitespace characters from the start or end');get_ipython().run_line_magic('pinfo', 'end')
+-->by using .lstrip() to remove whitespace characters from the start.
+-->by using .rstrip() to remove whitespace characters from the end.
+
+
+# In[38]:
+
+
+"   the".lstrip()
+
+
+# In[40]:
+
+
+"the         ".rstrip()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
